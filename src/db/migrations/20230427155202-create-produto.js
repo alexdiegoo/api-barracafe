d37871efd0149db3c632f4e33fe9fd17c8167083
alexdiegoo <alex.diegosn10@gmail.com>
2023-04-28 11:57:55 -0300

    criação das migrations

---
+++ src/db/migrations/20230427155202-create-produto.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Produtos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      imagem_link: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      descricao: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      preco: {
+        type: Sequelize.DECIMAL,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Produtos');
+  }
+};
