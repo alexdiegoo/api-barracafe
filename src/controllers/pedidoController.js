2a3fd981dd6e762adbd1df2051abddd9dcb6e687
alexdiegoo <alex.diegosn10@gmail.com>
2023-06-27 01:01:29 -0300

    buscar pedido por id

--- src/controllers/pedidoController.js
+++ src/controllers/pedidoController.js
@@ -1,6 +1,6 @@
 const models = require('../db/models');
 
-const { Pedido } = models;
+const { Pedido, Cliente, Produto } = models;
 
 class PedidoController {
   async index(req, res) {
@@ -14,6 +14,36 @@ class PedidoController {
         error: 'Erro interno no servidor',
         message: 'Estamos com problemas no servidor',
         path: '/pedidos/',
+      });
+    }
+  }
+
+  async get(req, res) {
+    const { id } = req.params;
+    try {
+      const pedido = await Pedido.findByPk(id, {
+        include: [
+          { model: Cliente, as: 'cliente' },
+          { model: Produto, as: 'produtos' },
+        ],
+      });
+
+      if (!pedido) {
+        return res.status(404).json({
+          status: 404,
+          error: 'Id do pedido inválido ou não existe',
+          message: 'Recurso não encontrado',
+          path: `/pedidos/${id}`,
+        });
+      }
+
+      return res.status(200).json({ pedido });
+    } catch (err) {
+      return res.status(500).json({
+        status: 500,
+        error: 'Erro interno no servidor',
+        message: 'Estamos com problemas no servidor',
+        path: `/pedidos/${id}`,
       });
     }
   }
--- src/routes/pedido.js
+++ src/routes/pedido.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const PedidoController = require('../controllers/pedidoController');
 
 router.get('/pedidos', PedidoController.index);
+router.get('/pedidos/:id', PedidoController.get);
 
 module.exports = router;
