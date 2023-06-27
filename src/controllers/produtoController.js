92d9abe762ce892b7550c77635e45fb3b4cefaee
alexdiegoo <alex.diegosn10@gmail.com>
2023-06-26 22:50:34 -0300

    buscar produto por id

--- src/controllers/produtoController.js
+++ src/controllers/produtoController.js
@@ -16,6 +16,30 @@ class ProdutoController {
       });
     }
   }
+
+  async get(req, res) {
+    try {
+      const produto = await Produto.findByPk(req.params.id);
+
+      if (!produto) {
+        return res.status(404).json({
+          status: 404,
+          error: 'Id do produto inválido ou não existe',
+          message: 'Recurso não encontrado',
+          path: `produtos/${req.params.id}`,
+        });
+      }
+
+      return res.status(200).json({ produto });
+    } catch (err) {
+      return res.status(500).json({
+        status: 500,
+        error: 'Erro interno no servidor',
+        message: 'Estamos com problemas no servidor',
+        path: `produtos/${req.params.id}`,
+      });
+    }
+  }
 }
 
 module.exports = new ProdutoController();
--- src/routes/produto.js
+++ src/routes/produto.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const ProdutoController = require('../controllers/produtoController');
 
 router.get('/produtos', ProdutoController.index);
+router.get('/produtos/:id', ProdutoController.get);
 
 module.exports = router;
